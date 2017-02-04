27d04c3fe654b097bd8d93021e688f0117640c3f
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-04 17:29:25 +0700

    tambah root auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   root 'welcome#index'
+  authenticated :user do
+    root 'notes#index', as: "authenticated_root"
+  end
   resources :notes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
