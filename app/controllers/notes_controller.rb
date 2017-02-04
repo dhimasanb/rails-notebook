0a765f397f9f52bb06cabb6d5c064110176bb3e7
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-04 15:27:02 +0700

    update new, create, note_params

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -6,9 +6,17 @@ class NotesController < ApplicationController
   end
 
   def new
+    @note = Note.new
   end
 
   def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to @note
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -26,5 +34,6 @@ class NotesController < ApplicationController
   end
 
   def note_params
+    params.require(:note).permit(:title, :content)
   end
 end
